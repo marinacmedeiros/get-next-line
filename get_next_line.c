cc8e64ebd3b59a63ec60d173dbf1160fbb2e4cae
Marina Medeiros <marinacmprojects@gmail.com>
2022-10-20 16:33:38 -0300

    up

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mamedeir <mamedeir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 16:21:36 by mamedeir          #+#    #+#             */
-/*   Updated: 2022/10/20 15:23:45 by mamedeir         ###   ########.fr       */
+/*   Updated: 2022/10/20 16:33:11 by mamedeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,33 @@
 
 char	*ft_cat_line(char *save)
 {
+	int		i;
+	char	*s;
 
+	i = 0;
+	if (!save[i])
+		return (NULL);
+	while (save[i] && save[i] != '\n')
+		i++;
+	s = (char *)malloc(sizeof(char) * (i + 2));
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (save[i] && save[i] != '\n')
+	{
+		s[i] = save[i];
+		i++;
+	}
+	if (save[i] == '\n')
+	{
+		s[i] = save[i];
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
 }
-
-
-
 
 char	*get_next_line(int fd)
 {
-	
+	int	c;
 }
