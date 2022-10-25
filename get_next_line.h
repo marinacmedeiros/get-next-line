019cd2ebc4504e760b57ad468282293f93076d27
Marina Medeiros <marinacmprojects@gmail.com>
2022-10-25 12:08:37 -0300

    firstat

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mamedeir <mamedeir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 16:21:40 by mamedeir          #+#    #+#             */
-/*   Updated: 2022/10/24 15:37:39 by mamedeir         ###   ########.fr       */
+/*   Updated: 2022/10/25 12:06:56 by mamedeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,13 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE
+# endif
 
 size_t	ft_strlen(const char *str);
-/* The strlen() function calculates the length of the string
- pointed to by s, excluding the terminating null byte ('\0').*/
 char	*ft_strchr(const char *s, int c);
-/*The strchr() function returns a pointer to the first occurrence
- of the character c in the string s.*/
 char	*ft_strjoin(const char *s1, const char *s2);
-/*Allocates (with malloc(3)) and returns a new string, 
- which is the result of the concatenation of ’s1’ and ’s2’.*/
 char	*ft_catch_line(char *store);
 char	*ft_save(char *store);
 char	*ft_read(int fd, char *store);
